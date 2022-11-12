9d6a250ec96ecda547eb84c124c0343ce660bb81
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-11 22:44:59 -0800

    solution added

--- LinkedListProgram/Program.cs
+++ LinkedListProgram/Program.cs
@@ -12,12 +12,11 @@ namespace LinkedListProgram
             LinkedList linkdedList = new LinkedList();
             linkdedList.Add(56);
             linkdedList.Add(30);
+            linkdedList.Add(40);
             linkdedList.Add(70);
             linkdedList.Display();
-            //linkdedList.DeleteLastNode();
-            // linkdedList.Display();
-            //linkdedList.Search(30);
-            linkdedList.InsertAtParticularPosition(3, 40);
+            
+            linkdedList.deleteAtParticularPosition(3, 40);
             linkdedList.Display();
         }
     }
